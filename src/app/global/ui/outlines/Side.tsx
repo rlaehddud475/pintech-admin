a9e214e7b49d2b5d575e8dd0fd4d7900f4f3f965
rlaehddud475 <6070kjh475@naver.com>
2025-02-07 18:09:49 +0900

    .

--- src/app/global/ui/outlines/Side.tsx
+++ src/app/global/ui/outlines/Side.tsx
@@ -1,8 +1,34 @@
 'use client'
 import React from 'react'
 import styled from 'styled-components'
-const StyledSideMenus = styled.aside``
+import sizes from '../../styles/sizes'
+import colors from '../../styles/colors'
+const { big } = sizes
+const { light, dark, white } = colors
+const StyledSideMenus = styled.aside`
+  min-height: 800px;
+  background: ${light};
+  a {
+    color: ${dark};
+    font-size: ${big};
+    display: block;
+    height: 45px;
+    line-height: 43px;
+    & .on{
+        background: ${dark};
+        color: ${white};
+    }
+    & + & {
+      board-top: 2px solid ${dark};
+      li
+    }
+  }
+`
 const Side = () => {
-  return <StyledSideMenus>사이드 메뉴</StyledSideMenus>
+  return (
+    <StyledSideMenus>
+      <a href=""></a>
+    </StyledSideMenus>
+  )
 }
 export default React.memo(Side)
