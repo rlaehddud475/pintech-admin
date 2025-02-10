40da9c3df56930f7c71f0988f0c98758546dbed8
rlaehddud475 <6070kjh475@naver.com>
2025-02-10 11:53:17 +0900

    .

--- src/app/board/config/components/ConfigList.tsx
+++ src/app/board/config/components/ConfigList.tsx
@@ -1,5 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+import { TableRows } from '@/app/global/components/Tables'
+import { Md10K, MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md'
+const StyledForm = styled.form``
 const ConfigList = () => {
-  return <h1>목록.......</h1>
+  return (
+    <>
+      <StyledForm>
+        <TableRows>
+          <thead>
+            <th>
+              <MdCheckBoxOutlineBlank />
+            </th>
+          </thead>
+        </TableRows>
+      </StyledForm>
+    </>
+  )
 }
 export default React.memo(ConfigList)
